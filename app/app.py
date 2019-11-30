3d6498784c1bf8403704dbc406c1508b285de68c
sverma92 <vermashivam679@gmail.com>
2019-11-29 21:12:35 -0800

    modified main app map

--- app/app.py
+++ app/app.py
@@ -7,6 +7,9 @@ import vega_datasets
 import numpy as np
 import data_info
 import pandas as pd
+import geopandas as gpd
+import json
+
 
 ### NEW IMPORT
 # See Docs here: https://dash-bootstrap-components.opensource.faculty.ai
@@ -20,7 +23,7 @@ app.title = 'Dash app with pure Altair HTML'
 
 import os as os
 x = os.getcwd()
-x = x[:len(x)-0]
+x = x[:len(x)-3]
 # Reading Vancouver City Population from 2001 to 2018
 path_pop=x + "/Data/Population_trend.csv"
 pop_yr = pd.read_csv(path_pop)
@@ -29,15 +32,86 @@ pop_yr = pop_yr[['YEAR', 'Population']]
 path_prop=x + "/Data/population_proportion.csv"
 pop_prop = pd.read_csv(path_prop)
 # >>>>    
+
+
+geo_json_file_loc= x + "/Data/local-area-boundary.geojson"
+def open_geojson():
+    with open(geo_json_file_loc) as json_data:
+        d = json.load(json_data)
+    return d
+
+def get_gpd_df():
+    vancouver_json = open_geojson()
+    gdf = gpd.GeoDataFrame.from_features((vancouver_json))
+    return gdf
+
+
+def create_geo_data(gdf):
+    choro_json = json.loads(gdf.to_json())
+    choro_data = alt.Data(values = choro_json['features'])
+    return choro_data
+
+# mapping function based on all of the above
+def gen_map_base(geodata, color_column):
+    '''
+        Generates Boston neighbourhoods map with building count choropleth
+    '''
+    # Add Base Layer
+    base = alt.Chart(geodata).mark_geoshape(
+        stroke='black',
+        strokeWidth=1
+    ).encode(
+    ).properties(
+        width=300,
+        height=300
+    )
+    return base
+
+def gen_map_cloro(geodata, geodata_2, color_column, tooltip):
+    # Add Choropleth Layer
+    choro = alt.Chart(geodata).mark_geoshape(
+        fill='lightgray',
+        stroke='black'
+    ).encode(
+        alt.Color(color_column, 
+                  scale=alt.Scale(), title = "Crime Counts"),
+         tooltip=tooltip
+    )
+
+    base = gen_map_base(geodata_2, color_column)
+    return base+choro
+
+
+def vancouver_map(df, df2):
+    vancouver_map = gen_map_cloro(geodata = df, geodata_2 = df2, 
+                        color_column='properties.N:Q'
+                       # color_scheme='yelloworangered'
+                        ,
+                        tooltip = [alt.Tooltip('properties.NEIGHBOURHOOD:O', title = 'Neighbourhood'),
+                                    alt.Tooltip('properties.N:Q', title = 'Crime Count')]
+    )
+    return vancouver_map
+
+
+
 # >>>> Changing function to take data as an input
 mydata = data_info.clean_data()
-
+gdf = get_gpd_df()
+gdf['NEIGHBOURHOOD'] = gdf['name']
+gdf = gdf.drop(columns="name")
+print(gdf.head())
 
 def make_charts(type_lst=['Break and Enter Commercial'], 
                 ngbrhd_lst=['Oakridge'], 
                 yr_lst=['2012']):
     df = mydata[(mydata['TYPE'].isin(type_lst)) & (mydata['NEIGHBOURHOOD'].isin(ngbrhd_lst)) & (mydata['YEAR'].isin(yr_lst))]
-    
+
+    df2 = df.groupby(by = 'NEIGHBOURHOOD')['NEIGHBOURHOOD'].agg([('N', 'count')]).reset_index()
+    gdf2 = gdf.merge(df2, on='NEIGHBOURHOOD', how='inner')
+
+    choro_data = create_geo_data(gdf2)
+    temp_chart = vancouver_map(choro_data, create_geo_data(gdf))
+
     MOY = df.groupby('MONTH')['MONTH'].agg([('N', 'count')]).reset_index().sort_values('MONTH', ascending=True)
     TOD = df.groupby('HOUR')['HOUR'].agg([('N', 'count')]).reset_index().sort_values('HOUR', ascending=True)
     type_crimes = df.groupby('TYPE')['TYPE'].agg([('N', 'count')]).reset_index().sort_values('N', ascending=False)
@@ -55,18 +129,21 @@ def make_charts(type_lst=['Break and Enter Commercial'],
     # >>>>
     
     charts = {}
-    charts[0] = alt.Chart(df).mark_point().encode(
-        x=alt.X('Lon:Q', title='Longitude', scale=alt.Scale(domain=[np.min(df.Lon), np.max(df.Lon)])),
-        y=alt.Y('Lat:Q', title='Latitude', scale=alt.Scale(domain=[np.min(df.Lat), np.max(df.Lat)])),
-        color = 'NEIGHBOURHOOD:N'
-    ).properties(
-        width=350,
-        height=300
-    ).interactive()
+    charts[0] = temp_chart
+    # charts[0] = alt.Chart(df).mark_point().encode(
+    #     x=alt.X('Lon:Q', title='Longitude', scale=alt.Scale(domain=[np.min(df.Lon), np.max(df.Lon)])),
+    #     y=alt.Y('Lat:Q', title='Latitude', scale=alt.Scale(domain=[np.min(df.Lat), np.max(df.Lat)])),
+    #     color = 'NEIGHBOURHOOD:N'
+    # ).properties(
+    #     width=350,
+    #     height=300
+    # ).interactive()
 
     charts[1] = alt.Chart(MOY).mark_bar().encode(
         x=alt.X('MONTH'),
-        y=alt.Y('N', title='Occurrence Count')
+        y=alt.Y('N', title='Occurrence Count'),
+        tooltip=[alt.Tooltip('N:Q', title = 'Occurrences'),
+                                    alt.Tooltip('MONTH:Q', title = 'Month')]
     ).properties(
         width=350,
         height=300
@@ -74,7 +151,9 @@ def make_charts(type_lst=['Break and Enter Commercial'],
 
     charts[2] = alt.Chart(TOD).mark_bar().encode(
         x=alt.X('HOUR'),
-        y=alt.Y('N', title='Occurrence Count')
+        y=alt.Y('N', title='Occurrence Count'),
+        tooltip=[alt.Tooltip('N:Q', title = 'Occurrences'),
+                                    alt.Tooltip('HOUR:Q', title = 'Hour')]
     ).properties(
         width=350,
         height=300
@@ -82,7 +161,9 @@ def make_charts(type_lst=['Break and Enter Commercial'],
 
     charts[3] = alt.Chart(crime_rate).mark_line().encode(
         x=alt.X('YEAR:O'),
-        y=alt.Y('rate', title='Crime Occurrences per 1000 People')
+        y=alt.Y('rate', title='Crime Occurrences per 1000 People'),
+        tooltip=[alt.Tooltip('rate:Q', title = 'Crime Rate'),
+                                    alt.Tooltip('YEAR:Q', title = 'Year')]
     ).properties(
         width=350,
         height=300 
@@ -136,11 +217,28 @@ selectors = dbc.Container([
                 dcc.Dropdown(
                     id='neighbour_dropdown',
                     options=[
-                        {'label': 'All Neigbourhoods', 'value': 'All'},
-                        {'label': 'Central Business District', 'value': 'Central Business District'},
-                        {'label': 'West End', 'value': 'West End'},
+                        {'label': 'Oakridge', 'value': 'Oakridge'},
                         {'label': 'Fairview', 'value': 'Fairview'},
-                        {'label': 'Mount Pleasant', 'value': 'Mount Pleasant'}
+                        {'label': 'West End', 'value': 'West End'},
+                        {'label': 'Central Business District', 'value': 'Central Business District'},
+                        {'label': 'Hastings-Sunrise', 'value': 'Hastings-Sunrise'},
+                        {'label': 'Strathcona', 'value': 'Strathcona'},
+                        {'label': 'Grandview-Woodland', 'value': 'Grandview-Woodland'},
+                        {'label': 'Kitsilano', 'value': 'Kitsilano'},                
+                        {'label': 'Kensington-Cedar Cottage', 'value': 'Kensington-Cedar Cottage'},                
+                        {'label': 'Sunset', 'value': 'Sunset'},                
+                        {'label': 'Mount Pleasant', 'value': 'Mount Pleasant'} ,
+                        {'label': 'Shaughnessy', 'value': 'Shaughnessy'},
+                        {'label': 'Marpole', 'value': 'Marpole'},                
+                        {'label': 'West Point Grey', 'value': 'West Point Grey'},                
+                        {'label': 'Victoria-Fraserview', 'value': 'Victoria-Fraserview'},                
+                        {'label': 'Kerrisdale', 'value': 'Kerrisdale'},                
+                        {'label': 'Riley Park', 'value': 'Riley Park'},
+                        {'label': 'Arbutus Ridge', 'value': 'Arbutus Ridge'},
+                        {'label': 'Renfrew-Collingwood', 'value': 'Renfrew-Collingwood'},
+                        {'label': 'Killarney', 'value': 'Killarney'},
+                        {'label': 'Dunbar-Southlands', 'value': 'Dunbar-Southlands'},
+                        {'label': 'South Cambie', 'value': 'South Cambie'}
                     ],
                     value=['Central Business District', 'Fairview'],
                     multi=True,
